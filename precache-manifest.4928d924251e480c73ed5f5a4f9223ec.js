4d3a6fc97e0458db0985674082f6252658840d0b
Ammarhr <hariry49@gmail.com>
2020-07-15 10:33:41 +0300

    Updates

---
+++ precache-manifest.4928d924251e480c73ed5f5a4f9223ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff5c4df59f27e1f53d595b062120ac7e",
+    "url": "//storefront/index.html"
+  },
+  {
+    "revision": "dd0242ec6809edee728b",
+    "url": "//storefront/static/js/2.d320b1d5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "//storefront/static/js/2.d320b1d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30cbebae386771ff1706",
+    "url": "//storefront/static/js/main.a44b7a83.chunk.js"
+  },
+  {
+    "revision": "a61e5a432764819b2a1c",
+    "url": "//storefront/static/js/runtime-main.6d644a3f.js"
+  }
+]);
